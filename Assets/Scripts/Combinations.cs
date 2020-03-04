c90e5428d4798224ffb42ab0e286cc77e9da49f1
Silwer0k <cupomaaax@gmail.com>
2020-03-04 16:11:19 +0300

    Поправил отслеживание фуллхауса

--- Assets/Scripts/Combinations.cs
+++ Assets/Scripts/Combinations.cs
@@ -13,10 +13,10 @@ public class Combinations : MonoBehaviour
 
         int threePairStatus = 0;
         int streetStatus = 0;
-        int fullHouseStatus = 0;
+        bool cleatPoints = false; 
         for (int i = 0; i < countsOfEachDiceState.Length; i++)
         {
-            if(countsOfEachDiceState[i] <= 2)
+            if(countsOfEachDiceState[i] <= 2 && countsOfEachDiceState[i] != 0)
             {
                 switch (i)
                 {
@@ -27,36 +27,51 @@ public class Combinations : MonoBehaviour
                         _points += 50 * countsOfEachDiceState[i];
                         break;
                     default:
+                        cleatPoints = true;
                         break;
                 }
-            }else
+            }else if (countsOfEachDiceState[i] != 0)
             {
                 var multiplier = (i == 0) ? 1000 : 100;
                 _points += (i + 1) * multiplier + ((i + 1) * multiplier) * (countsOfEachDiceState[i] - 3);
             }
-
-            if (countsOfEachDiceState.Length == 6)
-            {
-                threePairStatus = (countsOfEachDiceState[i] == 2) ? ++threePairStatus : threePairStatus + 0;
-                streetStatus = (countsOfEachDiceState[i] == 1) ? ++streetStatus : streetStatus + 0;
-                fullHouseStatus = (countsOfEachDiceState[i] == 4) ? ++fullHouseStatus : (countsOfEachDiceState[i] == 2) ? ++fullHouseStatus : fullHouseStatus + 0;
-            }
-            if (threePairStatus == 3)
-            {
-                _points = 750;
-                //extra roll
-            }
-            if (streetStatus == 6)
-            {
-                _points = 1500;
-                //extra roll
-            }
-            if (fullHouseStatus == 2)
-            {
-                _points = 1000;
-                //extra roll
-            }
+            threePairStatus = (countsOfEachDiceState[i] == 2) ? ++threePairStatus : threePairStatus + 0;
+            streetStatus = (countsOfEachDiceState[i] == 1) ? ++streetStatus : streetStatus + 0;
         }
+        if (threePairStatus == 3)
+        {
+            _points = 750;
+            //extra roll
+        }
+        if (streetStatus == 6)
+        {
+            _points = 1500;
+            //extra roll
+        }
+        bool isFullHouse = IsFullHouse(countsOfEachDiceState);
+        if (isFullHouse)
+        {
+            _points = 1000;
+            //extra roll
+        }
+        if (cleatPoints && threePairStatus != 3 && streetStatus != 6 && !isFullHouse)
+        {
+            _points = 0;
+        }
+    }
+
+    private bool IsFullHouse(int[] array)
+    {
+        bool pair = false;
+        bool four = false;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == 4)
+                four = true;
+            if (array[i] == 2)
+                pair = true;
+        }
+        return four && pair;
     }
 
     private int[] CountEachDiceState(List<Dice> dices)
