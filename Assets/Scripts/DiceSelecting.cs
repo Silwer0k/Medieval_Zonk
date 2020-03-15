8cf674f60951e491f9487ab68a8bb82a03ff429e
Silwer0k <cupomaaax@gmail.com>
2020-03-15 23:19:09 +0300

    Сделал выбор костей с помощью WASD и стрелочек

--- Assets/Scripts/DiceSelecting.cs
+++ Assets/Scripts/DiceSelecting.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DiceSelecting : MonoBehaviour
 {
     [SerializeField] private Combinations _combinations;
     [SerializeField] private Transform[] _heap;
+    [SerializeField] private float _nearnessRadius;
 
     private List<Dice> _selectedDices = new List<Dice>();
     private Transform _currentSelectedDice;
     private int _currentSelectedDiceIndex;
     private bool _pickAll = true;
+
+    private enum Sectors
+    {
+        Upper,
+        Left,
+        Down,
+        Right
+    }
 
     private void Start()
     {
@@ -23,9 +33,13 @@ public class DiceSelecting : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-            SelectNextDice(true);
+            SelectNextDice(_currentSelectedDice, Sectors.Upper);
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            SelectNextDice(false);
+            SelectNextDice(_currentSelectedDice, Sectors.Down);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            SelectNextDice(_currentSelectedDice, Sectors.Left);
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            SelectNextDice(_currentSelectedDice, Sectors.Right);
         if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return))
             PickDice(_currentSelectedDice);
         if (Input.GetKeyDown(KeyCode.Q))
@@ -74,10 +88,80 @@ public class DiceSelecting : MonoBehaviour
         SelectDice(_heap[_currentSelectedDiceIndex]);
     }
 
+    private void SelectNextDice(Transform currentSelectedDice, Sectors sector)
+    {
+        float distance = 100f;
+        Transform nextDice = null;
+        foreach (var dice in _heap)
+        {
+            if (dice != currentSelectedDice)
+            {
+                if (nextDice == null)
+                {
+                    nextDice = dice;
+                }
+                if (IsInSector(dice.position, currentSelectedDice.position, _nearnessRadius, sector))
+                {
+                    if(Vector2.Distance(currentSelectedDice.position, dice.position) < distance)
+                    {
+                        nextDice = dice;
+                        distance = Vector2.Distance(currentSelectedDice.position, dice.position);
+                    }
+                }
+            }
+        }
+        SelectDice(nextDice);
+    }
+
     private void SelectDice(Transform dice)
     {
         _currentSelectedDice.GetChild(1).GetComponent<SpriteRenderer>().enabled = false;
         dice.GetChild(1).GetComponent<SpriteRenderer>().enabled = true;
         _currentSelectedDice = dice;
+    }
+
+    private bool IsInSector(Vector2 dicePosition, Vector2 currentSelectedDicePos, float radius, Sectors sector)
+    {
+        Vector2 diceOffset = GetOffset(dicePosition, currentSelectedDicePos);
+        double polarAngle = Mathf.Atan2(diceOffset.y, diceOffset.x);
+        double polarRadius = Math.Sqrt(Math.Pow(diceOffset.x, 2) + Math.Pow(diceOffset.y, 2));
+        if (polarRadius > radius)
+        {
+            return false;
+        }
+        switch (sector)
+        {
+            case Sectors.Upper:
+                if (polarAngle >= Mathf.Atan2(radius, radius) && polarRadius <= Mathf.Atan2(radius, -radius)) return true;
+                break;
+            case Sectors.Left:
+                if ((polarAngle >= Mathf.Atan2(radius, -radius) && polarRadius <= Math.PI) || (polarAngle >= -Math.PI && polarAngle <= Mathf.Atan2(-radius, -radius))) return true;
+                break;
+            case Sectors.Down:
+                if (Mathf.Atan2(-radius, -radius) <= polarAngle && polarAngle <= Mathf.Atan2(-radius, radius)) return true;
+                break;
+            case Sectors.Right:
+                if ((polarAngle >= Mathf.Atan2(-radius, radius) && polarRadius <= 0) || (polarAngle >= 0 && polarAngle <= Mathf.Atan2(radius, radius))) return true;
+                break;
+        }
+        return false;
+    }
+
+    private Vector2 GetOffset(Vector3 position1, Vector3 position2)
+    {
+        return new Vector2(position1.x - position2.x, position1.y - position2.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        var dice = _currentSelectedDice;
+        if (_currentSelectedDice)
+        {
+            Handles.DrawWireDisc(dice.transform.position, new Vector3(0, 0, 1), _nearnessRadius);
+            Handles.DrawLine(dice.transform.position, new Vector3(dice.transform.position.x + _nearnessRadius, dice.transform.position.y + _nearnessRadius, 0));
+            Handles.DrawLine(dice.transform.position, new Vector3(dice.transform.position.x - _nearnessRadius, dice.transform.position.y + _nearnessRadius, 0));
+            Handles.DrawLine(dice.transform.position, new Vector3(dice.transform.position.x - _nearnessRadius, dice.transform.position.y - _nearnessRadius, 0));
+            Handles.DrawLine(dice.transform.position, new Vector3(dice.transform.position.x + _nearnessRadius, dice.transform.position.y - _nearnessRadius, 0));
+        }
     }
 }
